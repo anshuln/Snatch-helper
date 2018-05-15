32292bd6280d0de4806f3642c5da55b2f3c82b86
anshuln <38972131+anshuln@users.noreply.github.com>
2018-05-15 22:26:34 +0530

    Update UI.py

--- UI.py
+++ UI.py
@@ -1,11 +1,13 @@
 import Algos
-while (True):
+while True:
 	word=input("Enter a word \n")
 	word=word.upper()
-	choice=input("Enter 1 for anagrams, 2 for build \n")
+	choice=input("Enter 1 for anagrams, 2 for build , 3 for pattern \n")
 	if choice == '1':
 		Algos.anagram(word)
 	elif choice == '2':
 		Algos.build(word)
+	elif choice == '3':
+		Algos.pattern(word)
 	else:
 		break
